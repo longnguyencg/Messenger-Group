107ac0680b6f56d4b3a49aa5bd294498deb08fd9
long <thetachyon107@gmail.com>
2020-03-10 10:05:50 +0700

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatBoxComponent } from './chat-box/chat-box.component';
+import { LoginComponent } from './login/login.component';
+import { SearchUserComponent } from './chat-box/search-user/search-user.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import { ListUsersComponent } from './chat-box/list-users/list-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatBoxComponent,
+    LoginComponent,
+    SearchUserComponent,
+    ListUsersComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
